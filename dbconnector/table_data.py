4fe2885209c61e87ac9749f1433799b557bd9af7
Sachith <110117965+sacheex@users.noreply.github.com>
2023-11-11 19:31:30 +0530

    Update table_data.py

--- dbconnector/table_data.py
+++ dbconnector/table_data.py
@@ -4,8 +4,8 @@ def get_data(table_name, ):
     
     config = {
         "host": "localhost",
-        "user": "root",
-        "password": "84503580",
+        "user": "hr_manager",
+        "password": "hrpass",
         "database": "hr_department"
     }
 
